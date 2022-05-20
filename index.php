cb2c8d0e9dfc21828ee0350869e2873bc16f8043
LAPTOP-2G2OPN4P\maria <marianacalado0322@gmail.com>
2022-05-20 12:55:04 +0100

    isto é o ficheiro php que vamos fazer o website mas tem uns ex que estava a fazer

--- index.php
+++ index.php
@@ -5,7 +5,34 @@
         <title>Hello World</title>
     </head>
     <body>
-        <h1>Hello World!!!Ola</h1>
+        <h1><?php echo '1. Hello World!'?></h1>
         <a href="#teste">Test link!</a>
+        <p>
+            <?php echo//2
+                '2. How are you, world?<br>
+                I like you, world!<br>
+                You are my $world.<br>
+                Now, create a variable $world for the word “world” and echo the text to the browser,<br>
+                substituting the “world” word for “people” on the first two lines, but not on the last:
+                How are you, people?
+                I like you, people!
+                You are my $world';?>
+        </p> 
+        <?php 
+        $letters = array('F', 'E', 'U', 'P');
+        $text = implode('', $letters);
+        echo "3. ";
+        echo $text." is cool<br>";
+
+        $names = array('Alice', 'Jane', 'John');
+        //echo $names[1];
+        echo "4. John has two friends: $names[1] and $names[0].<br>";
+
+        $array = array ('Brand' => 'Ferrari', 'hp' => 500, 2 => 'wheels', 'mine');
+        foreach ($array as $key => $value)
+         echo "That $value with $key and 4 $value is $value";
+        ?>
+
     </body>
 </html>
+
