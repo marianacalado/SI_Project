ea1cc53ccf98432ee14ac6f095371782b4388a01
LAPTOP-2G2OPN4P\maria <marianacalado0322@gmail.com>
2022-05-26 11:07:28 +0100

    new2

--- index.php
+++ index.php
@@ -2,28 +2,25 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <!-- CSS Files MUDAR!!-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">  <!-- O QUE O PROF TINHA-->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- CSS Files MUDAR!!
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">  O QUE O PROF TINHA
         <link href="style.css" rel="stylesheet">
         <link href="layout.css" rel="stylesheet">
-        <link href="responsive.css" rel="stylesheet">
+        <link href="responsive.css" rel="stylesheet"> -->
         <title>MyPharma</title>
     </head>
     <body>
-        <h1><?php echo '1. Hello World!'?></h1>
-        <a href="#teste">Test link!</a>
-        <p>
-            <?php echo//2
-                '2. How are you, world?<br>
-                I like you, world!<br>
-                You are my $world.<br>
-                Now, create a variable $world for the word “world” and echo the text to the browser,<br>
-                substituting the “world” word for “people” on the first two lines, but not on the last:
-                How are you, people?
-                I like you, people!
-                You are my $world';?>
-        </p> 
         <?php 
+             echo "<h1>1. Hello World! </h1>";
+            
+            $world = "people";
+            echo "<p>2. How are you, $world? I like you, world!<br></p>";
+            echo '<p>You are my $world.<br></p>';
+ 
+         
         $letters = array('F', 'E', 'U', 'P');
         $text = implode('', $letters);
         echo "3. ";
@@ -34,8 +31,7 @@
         echo "4. John has two friends: $names[1] and $names[0].<br><br>";
                     
         $array = array ('Brand' => 'Ferrari', 'hp' => 500, 2 => 'wheels', 'mine');
-        foreach ($array as $value)
-         echo "5. That $value with $value and 4 $value is $value <br><br>";
+        echo "5. That $array[Brand] with $array[hp] and 4  $array[2] is $array[3] <br><br>";
 
         
          while($expre)
