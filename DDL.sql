cef587f36cd6390bef443de0f728f77b34dd76dd
LAPTOP-2G2OPN4P\maria <marianacalado0322@gmail.com>
2022-05-29 16:15:12 +0100

    alterei legendas que faltava traduzir

--- DDL.sql
+++ DDL.sql
@@ -143,15 +143,15 @@ INSERT INTO Suplier (id_suplier, name, phone_num, e_mail, address) VALUES (5, 'E
 INSERT INTO Suplier (id_suplier, name, phone_num, e_mail, address) VALUES (6, 'GE Healthcare Life Sciences', 225400600,'orderse@ge.com', 'Bjorkgatan 30 SE-75184 Uppsala Sweden');--este vai dar 4 produtos (ultimos, vai haver 1 que nao tem fornacedor)
 
 --Category
-INSERT INTO Category (id_category, name) VALUES (1, 'Beleza e higiene');
-INSERT INTO Category (id_category, name) VALUES (2, 'Cuidados Pessoais');
-INSERT INTO Category (id_category, name) VALUES (3, 'Medicamentos');
-INSERT INTO Category (id_category, name) VALUES (4, 'Suplementoa alimentares e nutrição');
-INSERT INTO Category (id_category, name) VALUES (5, 'Contraceção e sexualidade');
+INSERT INTO Category (id_category, name) VALUES (1, 'Beauty and Hygiene');
+INSERT INTO Category (id_category, name) VALUES (2, 'Personal Care');
+INSERT INTO Category (id_category, name) VALUES (3, 'Medicines');
+INSERT INTO Category (id_category, name) VALUES (4, 'Food supplements and Nutrition');
+INSERT INTO Category (id_category, name) VALUES (5, 'Contraception and intimate products');
 INSERT INTO Category (id_category, name) VALUES (6, 'Covid-19');
-INSERT INTO Category (id_category, name) VALUES (7, 'Equipamentos de saúde');
-INSERT INTO Category (id_category, name) VALUES (8, 'Saúde Animal');
-INSERT INTO Category (id_category, name) VALUES (9, 'Ortopédicos');--esta categoria não vai ter produtos
+INSERT INTO Category (id_category, name) VALUES (7, 'Medical Equipment');
+INSERT INTO Category (id_category, name) VALUES (8, 'Animal Care');
+INSERT INTO Category (id_category, name) VALUES (9, 'Orthopedic Products');--esta categoria não vai ter produtos
 
 --Brand
 INSERT INTO Brand (id_brand, name) VALUES (1, 'Giorgio Armani');
@@ -173,33 +173,33 @@ INSERT INTO Brand (id_brand, name) VALUES (15, 'Race'); -- 2 caes
 
 --Product
 --categoria, beleza e higiene e cuidado pessoal  
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (1, 'Acqua Di Gio', 65.99 ,'Perfume 100 mL aromático aquático masculino, marcado por notas cítricas e aquáticas, que revelam um frescor energizante único.', 1 , 1 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (2, 'Ideal Body Duche Spa Gel-Óleo', 20.00 ,'Gel de banho 200 mL enriquecido com ingredientes ativos e água mineralizante vichy, para uma hidratação instantânea e de longa duração.', 1 , 2 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (3, 'La Roche-Posay Desmaquilhante de Olhos Fisiológico', 10.00,'Desmaquilhante 125 mL suave para olhos sensíveis.', 1 , 3 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (4, 'Sensilis Perfect Line Liner de Lábios Rose', 8.89 ,'Lápis cor-de-rosa de textura cremosa que permite desenhar de forma perfeita o contorno dos lábios. Indicado para todo o tipo de peles.', 1 , 4 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (5, 'Aposán Recargas Lima Elétrica', 40.00 ,'Recargas para a Aposan Lima Elétrica que incluem 1 cabeça limadora, 2 cabeças polidoras e 2 cabeças abrilhantadoras.', 2 , 5 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (6, 'Listerine Dentes e Gengivas Elixir', 7.99 ,'Elixir 250 mL ideal para fortalecer o esmalte dos dentes, proteger e reforçar as gengivas, eliminando as bactérias que causam cáries, placa bacteriana, gengivite e mau hálito.', 2 , 6 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (7, 'Urgo Dor Plantar', 18.70,'Almofada protetora que contém um gel polímerico que alivia a dor, protege e isola o zona plantar de pressão e fricção do dia a dia.', 2 , 7 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (8, 'Venixe Fralda Média Noite', 14.09 ,'Fraldas com elásticos e de tamanho médio, indicadas para a incontinência muito grave. Com ajuste elástico, forma anatómica, camada absorvente, indicador de humidade, cobertura de falso tecido até às extremidades e barreiras laterais protetoras, 15 unidades.', 2 , 8 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (9, 'Urgo Verrugas Tratamento Crioterapia', 19.01 ,'Tratamento de crioterapia, para usar em casa, desenvolvido para o tratamento de verrugas comuns localizadas nas mãos e pés em adultos e crianças a partir dos 4 anos (20 ml).', 2 , 7 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (1, 'Acqua Di Gio', 65.99 ,'100 mL aromatic aquatic masculine perfume, marked by citrus and aquatic notes, which reveal a unique energizing freshness.', 1 , 1 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (2, 'Ideal Body Duche Spa Gel-Óleo', 20.00 ,'200 ml shower gel enriched with active ingredients and Vichy mineralizing water, for instant and long-lasting hydration.', 1 , 2 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (3, 'La Roche-Posay Desmaquilhante de Olhos Fisiológico', 10.00,'Make-up remover 125 ml gentle for sensitive eyes.', 1 , 3 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (4, 'Sensilis Perfect Line Liner de Lábios Rose', 8.89 ,'Pink pencil with a creamy texture that allows you to perfectly draw the contour of the lips. Suitable for all skin types.', 1 , 4 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (5, 'Aposán Recargas Lima Elétrica', 40.00 ,'Refills for Aposan Electric Lima that include 1 filer head, 2 polisher heads and 2 brightener heads.', 2 , 5 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (6, 'Listerine Dentes e Gengivas Elixir', 7.99 ,'Elixir 250 mL ideal for strengthening tooth enamel, protecting and strengthening gums, eliminating bacteria that cause cavities, plaque, gingivitis and halitosis.', 2 , 6 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (7, 'Urgo Dor Plantar', 18.70,'Protective pad that contains a polymeric gel that relieves pain, protects and isolates the plantar area from everyday pressure and friction.', 2 , 7 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (8, 'Venixe Fralda Média Noite', 14.09 ,'Diapers with elastic bands and of medium size, indicated for very severe incontinence. With elastic fit, anatomical shape, absorbent layer, moisture indicator, non-woven covering up to the edges and protective side barriers, 15 units.', 2 , 8 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (9, 'Urgo Verrugas Tratamento Crioterapia', 19.01 ,'Cryotherapy treatment, for use at home, developed for the treatment of common warts located on the hands and feet in adults and children from 4 years old (20 ml).', 2 , 7 );
 --medicamento sem receita
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (10, 'Sargenor 5, 5000 mg/10 mL x 20 amp beb', 15.99 ,'Medicamento indicado em situações de cansaço físico, psíquico ou sexual.', 3 , 9 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (11, 'Rennie Digestif, 680/80 mg x 24 comp mast', 3.99,'Medicamento indicado no alívio dos sintomas da azia e enfartamento.', 3 , 10 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (12, 'Aspirina C, 400/240 mg x 10 comp eferv', 5.40 ,'Medicamento indicado em caso de febre e dores de intensidade ligeira a moderada.', 3 , 10 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (13, 'Niquitin Clear, 14 mg/24 h x 14 sist transder', 56.01 ,'Medicamento indicado para quem pretende deixar de fumar.', 3 , 11 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (14, 'Niquitin Menta, 1,5 mg x 60 comp chupar', 32.95,'Medicamento indicado para quem pretende deixar de fumar.', 3 , 11 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (10, 'Sargenor 5, 5000 mg/10 mL x 20 amp beb', 15.99 ,'Medication indicated in situations of physical, psychological or sexual fatigue.', 3 , 9 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (11, 'Rennie Digestif, 680/80 mg x 24 comp mast', 3.99,'Medication indicated to relieve the symptoms of heartburn and bloating.', 3 , 10 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (12, 'Aspirine C, 400/240 mg x 10 comp eferv', 5.40 ,'Medication indicated in case of fever and pain of mild to moderate intensity.', 3 , 10 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (13, 'Niquitin Clear, 14 mg/24 h x 14 sist transder', 56.01 ,'Medication indicated for those who want to quit smoking.', 3 , 11 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (14, 'Niquitin Mint, 1,5 mg x 60 comp chupar', 32.95,'Medication indicated for those who want to quit smoking.', 3 , 11 );
 --medicamento com receita
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (15, 'Evacol, 7,5 mg/mL-30 mL x 1 sol oral gta, 7.5 mg/ml x 1 sol oral gta', 5.95 ,'Medicamento indicado no tratamento da obstipação (prisão de ventre).Evacol é um medicamento não sujeito a receita médica, indicado em casos de obstipação e em situações que requerem defecação facilitada.', 3 , 12 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (16, 'Doce Alívio x 30 comp', 6.95 ,'Medicamento indicado no tratamento de obstipações de diversas etiologias ou antes de exame ou cirurgia intestinal.', 3 , 12 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (17, 'Septolete Duo, 3/1 mg x 16 pst', 6.95 ,'Medicamento indicado no tratamento de obstipações de diversas etiologias ou antes de exame ou cirurgia intestinal.', 3 , 13 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (15, 'Evacol, 7,5 mg/mL-30 mL x 1 sol oral gta, 7.5 mg/ml x 1 sol oral gta', 5.95 ,'Medicine indicated in the treatment of constipation (constipation). Evacol is a non-prescription medicine indicated in cases of constipation and in situations requiring facilitated defecation.', 3 , 12 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (16, 'Doce Alívio x 30 comp ', 6.95 ,'Medication indicated in the treatment of constipation of various etiologies or before intestinal examination or surgery.', 3 , 12 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (17, 'Septolete Duo, 3/1 mg x 16 pst', 6.95 ,'Medication indicated in the treatment of constipation of various etiologies or before intestinal examination or surgery.', 3 , 13 );
 --suplementos ate saude animal --categoria orto não vai ter produtos!!!
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (18, 'Dulcosoft Pó Solulçao Oral Saquetas 10 G x20', 13.95 ,'Pó para solução oral indicado para amolecer as fezes duras e secas de forma a facilitar a evacuação.', 4 , 12 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (19, 'Durex Natural Plus Preservativo', 3.95 ,'Preservativos, fáceis de colocar que que proporcionam conforto ao usar.', 5 , 12 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (20, 'Máscaras Cirúrgicas IIR Criança Freedom Azul Caixa x50', 3.99 ,'Máscaras cirúrgicas tipo IRR de criança.Embalagem com 50 unidades.', 6 , 13 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (21, 'Ladvance Álcool Gel Desinfetante 5L', 22.50 ,'Todos os tipos de pele.Uso Familiar.70% de álcool etílico.Gel desinfetante cutâneo que confere sensação de frescura e bem-estar da pele.', 6 , 13 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (22, 'Flocath Hydrogel Cateter 851121 CH 12', 3.99 ,'Cateter de hidrogel, com ponta tipo nelaton e duas entradas, indicado para mulher.', 7 , 3 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (23, 'Wejoint Comprimidos X30 Cão Grande', 21.30 ,'Alimento complementar para cães e gatos desenvolvido para o auxílio e suporte nutricional das articulações. Recomendado para animais jovens em crescimento, animais com elevada actividade física, e em qualquer tipo de patologia articular (inflamatória, traumática, degenerativa ou onde haja situações de imobilização articular).', 8 , 15 );
-INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (24, 'Omniomega Cápsulas X120', 38.99 ,'Suplemento alimentar para cães e gatos com elevado teor de Ómega-3 e vitamina E. Omniomega apresenta benefícios em patologias dérmicas, cardiovasculares, renais, neurais, ortopédicas e oncológicas', 8 , 15 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (18, 'Dulcosoft Pó Solulçao Oral Saquetas 10 G x20', 13.95 ,'Powder for oral solution indicated to soften hard and dry stools to facilitate evacuation.', 4 , 12 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (19, 'Durex Natural Plus Preservativo', 3.95 ,'Condoms, easy to put on that provide comfort to use.', 5 , 12 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (20, 'Máscaras Cirúrgicas IIR Criança Freedom Azul Caixa x50', 3.99 ,'Children IRR surgical masks. Pack of 50 units.', 6 , 13 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (21, 'Ladvance Álcool Gel Desinfetante 5L', 22.50 ,'All skin types. Family use. 70% ethyl alcohol. Skin disinfectant gel that gives the skin a feeling of freshness and well-being.', 6 , 13 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (22, 'Flocath Hydrogel Cateter 851121 CH 12', 3.99 ,'Hydrogel catheter, with delaton tip and two ports, suitable for women.', 7 , 3 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (23, 'Wejoint Comprimidos X30 Cão Grande', 21.30 ,'Complementary food for dogs and cats developed for the aid and nutritional support of the joints. Recommended for young growing animals, animals with high physical activity, and in any type of joint pathology (inflammatory, traumatic, degenerative or where there are situations of joint immobilization).', 8 , 15 );
+INSERT INTO Product (id_product, name, unit_price, description, category_id, brand_id) VALUES (24, 'Omniomega Capsules X120', 38.99 ,'Food supplement for dogs and cats with a high content of Omega-3 and vitamin E. Omniomega has benefits in dermal, cardiovascular, renal, neural, orthopedic and oncological pathologies.', 8 , 15 );
 
 
 --SupProduct (o suplier 1 nao tem produtos e o produto 25 nao tem sup)
