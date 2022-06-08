0a6b548cdd7cebd11f566eeaffd555fa4857fc48
DESKTOP-UUBK3FF\SusyC <up202103376@up.pt>
2022-06-08 18:07:48 +0100

    alteracoes header!!!

--- template/header2_tem.php
+++ template/header2_tem.php
@@ -1,10 +1,18 @@
-<header>
-    <a href="./init_page.php"><img alt="Logo" src="./assets/Logo_white.png"/></a>
-    <!-- <div id="logout">
-    <a href="./init_page.php">Logout</a>
-    </div> -->
-    <form id="logout" action="action_logout.php">
-        <span><?php echo $_SESSION["email"] ?></span>
-        <input type="submit" value="Logout">
-    </form>
-</header>
+<!DOCTYPE html>
+
+<head> 
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="header2.css" rel="stylesheet" />
+</head>
+
+<body>
+    <header>
+        <a href="./init_page.php"><img alt="Logo" src="./assets/Logo_white.png"/></a>
+        <form id="logout" action="action_logout.php">
+            <span><?php echo $_SESSION["email"] ?></span>
+            <input type="submit" value="Logout">
+        </form>
+    </header>
+</body>
